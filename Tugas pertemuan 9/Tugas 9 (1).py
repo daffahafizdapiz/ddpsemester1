6172247edbe9dfd14a38b51c96e55338d2e92135
daffahafizdapiz <146185809+daffahafizdapiz@users.noreply.github.com>
2023-11-23 12:58:58 +0700

    Add files via upload

---
+++ Tugas pertemuan 9/Tugas 9 (1).py
@@ -0,0 +1,9 @@
+def profil(nama, alamat, gender, umur, hobi):
+    print("Data Diri")
+    print("Nama\t:",nama)
+    print("Alamat\t:", alamat)
+    print("Gender\t:", gender)
+    print("Umur\t:", umur, "Tahun")
+    print("Hobi\t:", hobi)
+
+profil("Daffa", "Desa Tarikolot Rt.01/Rw.07", "Laki-laki", "18", "Futsal")    
---
+++ Tugas pertemuan 9/Tugas 9 (3).py
@@ -0,0 +1,6 @@
+def ganjil(n):
+    print("Bilangan Ganjil:")
+    for i in range(1, n+1, 2):
+        print(i)
+
+ganjil(50)        
